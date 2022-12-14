8132ea97ad2ae5af9e76a5d8d930dbc2bd68a1ff
Matey-Nikolov <matey.nikolov@mail.bg>
2022-12-14 12:26:07 +0200

    Update

--- HouseRentingSystem/Data/DataConstants.cs
+++ HouseRentingSystem/Data/DataConstants.cs
@@ -2,5 +2,16 @@
 {
     public class DataConstants
     {
+        public class House
+        {
+            public const int TitleMaxLength = 50;
+            public const int AddressMaxLength = 150;
+            public const int DescriptionMaxLength = 500;
+        }
+
+        public class Category
+        {
+            public const int NameMaxLength = 50;
+        }
     }
 }
--- HouseRentingSystem/Data/Entities/House.cs
+++ HouseRentingSystem/Data/Entities/House.cs
@@ -1,4 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static HouseRentingSystem.Data.DataConstants.House;
 
 namespace HouseRentingSystem.Data.Entities
 {
@@ -7,17 +9,21 @@ namespace HouseRentingSystem.Data.Entities
         public int Id { get; set; }
 
         [Required]
+        [MaxLength(TitleMaxLength)]
         public string Title { get; set; }
 
         [Required]
+        [MaxLength(AddressMaxLength)]
         public string Address { get; set; }
 
         [Required]
+        [MaxLength(DescriptionMaxLength)]
         public string Description { get; set; }
 
         [Required]
         public string ImageUrl { get; set; }
 
+        [Column(TypeName = "decimal(12, 3)")]
         public decimal PricePerMonth { get; set; }
 
         public int CategoryId { get; set; }
