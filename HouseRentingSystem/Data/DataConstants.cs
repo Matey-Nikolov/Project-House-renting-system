8188fcd9cde310e1cf9c5b7f8ca88427ef88212d
Matey-Nikolov <matey.nikolov@mail.bg>
2022-12-14 12:16:25 +0200

    House class

---
+++ HouseRentingSystem/Data/DataConstants.cs
@@ -0,0 +1,6 @@
+﻿namespace HouseRentingSystem.Data
+{
+    public class DataConstants
+    {
+    }
+}
---
+++ HouseRentingSystem/Data/Entities/House.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HouseRentingSystem.Data.Entities
+{
+    public class House
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+
+        public decimal PricePerMonth { get; set; }
+
+        public int CategoryId { get; set; }
+        //public Category Category { get; set; }
+
+        public int AgenId { get; set; }
+        //public Agent Agent { get; set; }
+
+        public string RenterId { get; set; }
+    }
+}
