fdfd4d1c0c28af215555c5348d3db4c90acb87d6
Matey-Nikolov <matey.nikolov@mail.bg>
2022-12-22 20:28:39 +0200

    update

--- HouseRentingSystem/Controllers/AgentsController.cs
+++ HouseRentingSystem/Controllers/AgentsController.cs
@@ -1,10 +1,19 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using HouseRentingSystem.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HouseRentingSystem.Controllers
 {
     public class AgentsController : Controller
     {
+        private readonly HouseRentingDbContext data;
+
+
+        public AgentsController(HouseRentingDbContext data)
+        {
+            this.data = data;
+        }
+
         [HttpPost]
         [Authorize]
         public IActionResult Become()
