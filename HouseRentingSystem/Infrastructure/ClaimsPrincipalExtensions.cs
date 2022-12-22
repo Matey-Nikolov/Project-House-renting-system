69952133bfb8126476aee3fcfc38cded2e1d483c
Matey-Nikolov <matey.nikolov@mail.bg>
2022-12-22 20:31:39 +0200

    update

---
+++ HouseRentingSystem/Infrastructure/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace HouseRentingSystem.Infrastructure
+{
+    public class ClaimsPrincipalExtensions
+    {
+        public static string Id(ClaimsPrincipal user)
+        {
+            return user.FindFirst(ClaimTypes.NameIdentifier).Value;
+        }
+    }
+}
